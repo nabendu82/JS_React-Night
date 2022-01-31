b52f7c2edd432e1f353f6e2093b77c7f351c9717
Nabendu Biswas <nabendu.biswas@gmail.com>
2022-01-31 23:15:27 +0530

    React class based

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,14 @@
 import './App.css';
-import PersonList from './components/PersonList';
-// import Counter from './components/Counter';
-// import { Greet, Hello } from './components/Greet';
-// import Greeting from './components/Greeting';
-// import Welcome from './components/Welcome';
+import GrandParent from './components/GrandParent';
+import { UserProvider } from './components/userContext';
 
 function App() {
+  
   return (
     <div className="App">
-      {/* <Greet />
-      <Hello /> */}
-      {/* <Greeting name="JavaScript">Language of the Web</Greeting> 
-      <Greeting name="ReactJS" />
-      <Greeting name="Swift">Language for ios apps</Greeting>
-      <hr />
-      <Welcome name="Flutter">Language for cross-platform mobile development</Welcome>
-      <Welcome name="React Native" />
-      <Welcome name="Java"><button>Click</button></Welcome> */}
-      {/* <Counter /> */}
-      <PersonList />
+      <UserProvider value="Nabendu">
+        <GrandParent />
+      </UserProvider>
     </div>
   );
 }
